8c841af1e30f797a9cb8caffa56bb7c549cc72e2
christopherivy <cdi.747@gmail.com>
2022-02-11 13:12:08 -0800

    shopping list start

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const app = express();
+const itemsRoutes = require("./routes/items")
+const ExpressError = require("./expressError")
+
+app.use(express.json());
+app.use("/items", itemsRoutes);
